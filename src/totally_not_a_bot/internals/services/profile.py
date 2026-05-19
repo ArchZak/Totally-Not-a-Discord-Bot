dc65c2229853ff01a351034706878e07b76cfd2d
ArchZak <izear21@gmail.com>
2026-05-18 22:29:00 -0400

    docs: small docs change for profile service

--- src/totally_not_a_bot/internals/services/profile.py
+++ src/totally_not_a_bot/internals/services/profile.py
@@ -2,6 +2,15 @@
 
 
 def set_user_status(status: str):
+    """
+    Set the status of the discord bot.
+
+    Args:
+        status (str): The new status for the bot user.
+
+    Returns:
+        None
+    """
     pass
 
 
