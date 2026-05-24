22090235b2f2093e968b89e2bd25a924c5d4328f
ArchZak <izear21@gmail.com>
2026-05-23 20:51:05 -0400

    feat: implemented profile services to set activity and status

---
+++ src/totally_not_a_bot/internals/dto/profiles_dto.py
@@ -0,0 +1,34 @@
+from server import _client
+
+# region Profile Tools
+
+
+async def set_user_status_dto(status: str):
+    """
+    Set the status of the discord bot.
+
+    Args:
+        status (str): The new status for the bot user
+
+    Returns:
+        None
+    """
+    await _client.change_presence(activity=_client.activity, status=status)
+
+
+async def set_user_activity_dto(activity_type: str, activity_name: str):
+    """
+    Set the activity of the discord bot.
+
+    Args:
+        activity_type (str): The type of activity (e.g., "playing", "streaming", "listening", "watching")
+        activity_name (str): The name of the activity
+
+    Returns:
+        None
+    """
+    activity = _client.Activity(type=activity_type, name=activity_name)
+    await _client.change_presence(activity=activity)
+
+
+# endregion
--- src/totally_not_a_bot/internals/services/profile_services.py
+++ src/totally_not_a_bot/internals/services/profile_services.py
@@ -1,7 +1,11 @@
+from typing import Annotated, Literal
+
+from dto import profiles_dto
+
 # region Profile Tools
 
 
-def set_user_status(status: str):
+async def set_user_status(status: str):
     """
     Set the status of the discord bot.
 
@@ -11,7 +15,28 @@ def set_user_status(status: str):
     Returns:
         None
     """
-    pass
+    await profiles_dto.set_user_status_dto(status)
+
+
+async def set_user_activity(
+    activity_type: Annotated[
+        str,
+        Literal["playing", "streaming", "listening", "watching"],
+        'The type of activity (e.g., "playing", "streaming", "listening", "watching")',
+    ],
+    activity_name: str,
+):
+    """
+    Set the activity of the discord bot.
+
+    Args:
+        activity_type (str): The type of activity (e.g., "playing", "streaming", "listening", "watching")
+        activity_name (str): The name of the activity
+
+    Returns:
+        None
+    """
+    await profiles_dto.set_user_activity_dto(activity_type, activity_name)
 
 
 # endregion
